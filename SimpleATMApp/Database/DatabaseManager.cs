59464b3d8b6dfe6ad5ea4c82efba700f762a8eab
soniapop93 <popsonia85@gmail.com>
2023-04-27 18:46:07 +0300

    started to work on the database manager

---
+++ SimpleATMApp/Database/DatabaseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SQLite;
+
+namespace SimpleATMApp.Database
+{
+    public class DatabaseManager
+    {
+        private SQLiteConnection sqLiteConnection;
+
+        public DatabaseManager()
+        {
+            generateDB("C:\\Users\\" + System.Environment.UserName + "\\Desktop", "bankDB");
+        }
+        private void generateDB(string path, string fileName)
+        {
+            string fileNamePath = path + @"\" + fileName + ".db";
+            createConnection(fileNamePath);
+
+            Console.WriteLine("Database is created successfully");
+            createTable();
+        }
+
+        private void createConnection(string fileNamePath)
+        {
+            string strConnection = String.Format("Data Source={0};Version=3;New=True;Compress=True;", fileNamePath);
+            sqLiteConnection = new SQLiteConnection(strConnection);
+        }
+
+        private void createTable()
+        {
+            string strSql = ""; //TODO: implement db columns
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strSql;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+    }
+}
