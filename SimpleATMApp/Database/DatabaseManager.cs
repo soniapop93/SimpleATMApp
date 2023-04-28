8e93c631478a621bceaeb2217184aa80bd11a44f
soniapop93 <popsonia85@gmail.com>
2023-04-28 13:50:32 +0300

    implemented the columns for users, card, transactions database

--- SimpleATMApp/Database/DatabaseManager.cs
+++ SimpleATMApp/Database/DatabaseManager.cs
@@ -8,7 +8,7 @@ namespace SimpleATMApp.Database
 
         public DatabaseManager()
         {
-            generateDB("C:\\Users\\" + System.Environment.UserName + "\\Desktop", "bankDB");
+            generateDB("C:\\Users\\" + System.Environment.UserName + "\\Desktop", "ATMbankDB");
         }
         private void generateDB(string path, string fileName)
         {
@@ -16,7 +16,9 @@ namespace SimpleATMApp.Database
             createConnection(fileNamePath);
 
             Console.WriteLine("Database is created successfully");
-            createTable();
+            createTableUsers();
+            createTableCards();
+            createTableTransactions();
         }
 
         private void createConnection(string fileNamePath)
@@ -25,9 +27,52 @@ namespace SimpleATMApp.Database
             sqLiteConnection = new SQLiteConnection(strConnection);
         }
 
-        private void createTable()
+        private void createTableUsers()
         {
-            string strSql = ""; //TODO: implement db columns
+            string strSql = "CREATE TABLE IF NOT EXISTS Users " +
+                "(first_name TEXT," +
+                "last_name TEXT," +
+                "address TEXT," +
+                "country TEXT," +
+                "city TEXT," +
+                "mobile_phone TEXT," +
+                "email TEXT," +
+                "card_id INT," +
+                "cash_availability TEXT," +
+                "limitWithdrawal INT," +
+                "transaction_ids TEXT)"; 
+            
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strSql;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        private void createTableCards()
+        {
+            string strSql = "CREATE TABLE IF NOT EXISTS Cards " +
+                "(userID INT," +
+                "card_number TEXT," +
+                "card_type TEXT," +
+                "expiration_date TEXT," +
+                "pin TEXT)";
+
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strSql;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        private void createTableTransactions()
+        {
+            string strSql = "CREATE TABLE IF NOT EXISTS Transactions " +
+                "(userID INT," +
+                "transaction_date TEXT," +
+                "transaction_ammount TEXT," +
+                "transaction_currency TEXT)";
+
             sqLiteConnection.Open();
             SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
             sqLiteCommand.CommandText = strSql;
