f8a2e10cdb459d02ed59bb29a761d87e9b7bc0a8
soniapop93 <popsonia85@gmail.com>
2023-04-29 22:16:25 +0300

    started to implement the functions that inserts data in table db

--- SimpleATMApp/Database/DatabaseManager.cs
+++ SimpleATMApp/Database/DatabaseManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SQLite;
+﻿using SimpleATMApp.User;
+using System.Data.SQLite;
 
 namespace SimpleATMApp.Database
 {
@@ -30,7 +31,8 @@ namespace SimpleATMApp.Database
         private void createTableUsers()
         {
             string strSql = "CREATE TABLE IF NOT EXISTS Users " +
-                "(first_name TEXT," +
+                "(user_id INT PRIMARY KEY," +
+                "first_name TEXT," +
                 "last_name TEXT," +
                 "address TEXT," +
                 "country TEXT," +
@@ -52,7 +54,8 @@ namespace SimpleATMApp.Database
         private void createTableCards()
         {
             string strSql = "CREATE TABLE IF NOT EXISTS Cards " +
-                "(userID INT," +
+                "(card_id INT PRIMARY KEY," +
+                "user_id INT," +
                 "card_number TEXT," +
                 "card_type TEXT," +
                 "expiration_date TEXT," +
@@ -68,7 +71,8 @@ namespace SimpleATMApp.Database
         private void createTableTransactions()
         {
             string strSql = "CREATE TABLE IF NOT EXISTS Transactions " +
-                "(userID INT," +
+                "(transaction_id INT PRIMARY KEY," +
+                "user_id INT," +
                 "transaction_date TEXT," +
                 "transaction_ammount TEXT," +
                 "transaction_currency TEXT)";
@@ -76,6 +80,81 @@ namespace SimpleATMApp.Database
             sqLiteConnection.Open();
             SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
             sqLiteCommand.CommandText = strSql;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        public void insertDataUsers(UserDetails user)
+        {
+            string strData = "INSERT INTO Users " +
+                "(first_name TEXT," +
+                "last_name TEXT," +
+                "address TEXT," +
+                "country TEXT," +
+                "city TEXT," +
+                "mobile_phone TEXT," +
+                "email TEXT," +
+                "card_id INT," +
+                "cash_availability TEXT," +
+                "limitWithdrawal INT," +
+                "transaction_ids TEXT) VALUES " +
+                "('" +
+                user.personalInformation.firstName + "','" +
+                user.personalInformation.lastName + "','" +
+                user.personalInformation.address + "','" +
+                user.personalInformation.country + "','" +
+                user.personalInformation.city + "','" +
+                user.personalInformation.mobilePhone + "','" +
+                user.personalInformation.email + "'," + 
+                user.card.cardID + ",'" + 
+                user.cashAvailability + "'," + 
+                user.limitWithdrawal + ",'" + 
+                String.Join(",", user.transactions) + "');";
+
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strData;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        public void insertDataCards(UserDetails user)
+        {
+            string strData = "INSERT INTO Cards " +
+               "(user_id INT," + 
+               "card_number TEXT," +
+               "card_type TEXT," +
+               "expiration_date TEXT," +
+               "pin TEXT) VALUES " +
+               "(" +
+               user.userID + ",'" +
+               user.card.cardNumber + "','" +
+               user.card.cardType + "','" +
+               user.card.expirationDate + "','" +
+               user.card.pin + "');";
+
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strData;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        public void insertDataTransactions(UserDetails user, DateTime transactionDate, string transactionAmmount, string transactionCurrency)
+        {
+            string strData = "INSERT INTO Transactions " +
+               "(user_id INT," +
+               "transaction_date TEXT," +
+               "transaction_ammount TEXT," +
+               "transaction_currency TEXT) VALUES " +
+               "(" +
+               transactionDate + ",'" +
+               transactionAmmount + "','" +
+               transactionCurrency + "');";
+
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strData;
             sqLiteCommand.ExecuteNonQuery();
             sqLiteConnection.Close();
         }
--- SimpleATMApp/User/Card.cs
+++ SimpleATMApp/User/Card.cs
@@ -2,13 +2,15 @@
 {
     public class Card
     {
+        public int cardID { get; set; }
         public string cardNumber { get; set; }
         public string cardType { get; set; }
         public DateTime expirationDate { get; set; }
         public int pin { get; set; }
 
-        public Card(string cardNumber, string cardType, DateTime expirationDate, int pin)
+        public Card(int cardID, string cardNumber, string cardType, DateTime expirationDate, int pin)
         {
+            this.cardID = cardID;
             this.cardNumber = cardNumber;
             this.cardType = cardType;
             this.expirationDate = expirationDate;
